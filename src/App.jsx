d557c0fb3cc4869380593b0770d993f2697f65e8
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-24 22:45:39 +0600

    Basic setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
-
+import Button from '@mui/material/Button';
 function App() {
 
   return (
     <>    
-    <h1>Welcome</h1>    </>
+    <h1 className='text-primary'>Welcome</h1>    
+    <h1 className='text-primary text-6xl'>Welcome</h1>    
+    <h1 className='bg-background'>Welcome</h1>    
+    <h1 className='text-primary'>Welcome</h1>    
+    <h1 className='text-primary'>Welcome</h1>  
+    <Button variant="contained" color="primary">
+      Click Me
+    </Button>  
+    </>
   )
 }
 
